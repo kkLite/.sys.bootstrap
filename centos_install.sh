f7d8908ca1d021e2611ea323f0c7979546f7eb1e
fengkeke <fengkeke@u51.com>
2019-11-12 14:19:18 +0800

    [MDF]修改安装脚本

--- centos_install.sh
+++ centos_install.sh
@@ -1,6 +1,25 @@
+if !hasInstall_oh_my_zsh; then
+    sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi 
+
+if $?
+then
 #1. 安装diff辅助工具 https://github.com/jeffkaufman/icdiff
 pip install git+https://github.com/jeffkaufman/icdiff.git
 #2. 安装oh my zsh 插件 代码高亮插件 默认安装到oh my zsh的自定义插件目录，如果安装其他目录，则需要在~/.zshrc更改路径
-cd ~/.oh-my-zsh/custom/plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+yum install autojump-zsh 
+
+fi
+
+# 判断是否安装了oh my zsh
+ function hasInstall_oh_my_zsh() {
+   if [ ! -d "$HOME/.oh-my-zsh" ]; then
+        echo 'oh-my-zsh 没有安装, 自动安装中...'
+        return 0
+    else
+        echo 'oh-my-zsh 已经安装了'
+        return 1
+    fi
+ }
 
--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ then
 pip install git+https://github.com/jeffkaufman/icdiff.git
 #2. 安装oh my zsh 插件 代码高亮插件 默认安装到oh my zsh的自定义插件目录，如果安装其他目录，则需要在~/.zshrc更改路径
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+brew install autojump
+
 fi
 
 # 判断是否安装了oh my zsh
