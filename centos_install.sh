b56ae14edd919acb486ec7e285e0e5b4a0cf349a
fengkeke <fengkeke@u51.com>
2019-11-12 14:56:10 +0800

    [MDF]修改错误的脚本

--- centos_install.sh
+++ centos_install.sh
@@ -1,16 +1,16 @@
-if !hasInstall_oh_my_zsh; then
+hasInstall_oh_my_zsh 
+if [$? -gt 0]; then
     sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi 
 
-if $?
+if [$? -eq 0]
 then
 #1. 安装diff辅助工具 https://github.com/jeffkaufman/icdiff
 pip install git+https://github.com/jeffkaufman/icdiff.git
 #2. 安装oh my zsh 插件 代码高亮插件 默认安装到oh my zsh的自定义插件目录，如果安装其他目录，则需要在~/.zshrc更改路径
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-yum install autojump-zsh 
-
+yum install autojump-zsh
 fi
 
 # 判断是否安装了oh my zsh
